fb1f9f5a7060df22bc787cffb33e88aa875fa830
Csongor Zonda <csongor@boam.com>
2023-01-04 15:17:33 +0100

    dirty

--- packages/web-app/pages/index.tsx
+++ packages/web-app/pages/index.tsx
@@ -4,7 +4,7 @@ import { Header } from '@project/components';
 export default function Index() {
   return (
     <Header>
-      Header goes here
+      Header goes here test
     </Header>
   );
 }
